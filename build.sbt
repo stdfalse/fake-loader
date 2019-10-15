ccdc6a8adb794525df79f85d0232874e86047040
Egor Abramov <egor@snowplowanalytics.com>
2019-10-15 12:25:54 +0700

    Added an argument

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "fake-loader"
 version := "0.1"
 
 scalaVersion := "2.12.9"
+
+libraryDependencies += "com.monovore" %% "decline" % "1.0.0"
--- src/main/scala/com/snowplowanalytics/snowplow/fakeloader/Main.scala
+++ src/main/scala/com/snowplowanalytics/snowplow/fakeloader/Main.scala
@@ -1,19 +1,26 @@
 package com.snowplowanalytics.snowplow.fakeloader
 
 import scala.io.Source
+import cats.implicits._
+import com.monovore.decline._
 
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    val bufferedSource = Source.fromFile("src/resources/test.txt")
+object Main extends CommandApp(
+  name = "fakeloader",
+  header = "Reads a file",  
+  main = {
+    val destOpt = Opts.option[String]("destination", help = "Path to a file")
+
+    destOpt.map { (destination) =>
+      println(s"$destination")
+    }
+
+    /*val bufferedSource = Source.fromFile("src/resources/test.txt")
     bufferedSource.getLines.foreach { line =>
       println(line.toUpperCase)
     }
-
-    args.foreach { arg =>
-    	println(arg)
-    }
-
     bufferedSource.close()
+
+    */
   }
-}
+)
